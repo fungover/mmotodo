e3c7dc51ae91e4379262cfc01c8e1d18d3e99f56
Martin Blomberg <martin.blomberg@outlook.com>
2023-12-04 09:44:34 +0100

    Initial commit

---
+++ src/main/java/org/fungover/mmotodo/MmotodoApplication.java
@@ -0,0 +1,13 @@
+package org.fungover.mmotodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmotodoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MmotodoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/fungover/mmotodo/MmotodoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fungover.mmotodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MmotodoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
