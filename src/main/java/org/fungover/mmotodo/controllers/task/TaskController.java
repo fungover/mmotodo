bca83047b66e4df81d5546cc3379ab5eda499308
Axel Asp <solidtop.asp@gmail.com>
2023-12-07 11:22:03 +0100

    Setup task controller & service
    Co-authored-by: Jakob Palmborg <112066906+jakobpalmborg@users.noreply.github.com>
    Co-authored-by: Fabricio Flores <32011023+rfabricioflores@users.noreply.github.com>

---
+++ src/main/java/org/fungover/mmotodo/controllers/task/TaskController.java
@@ -0,0 +1,32 @@
+package org.fungover.mmotodo.controllers.task;
+
+import org.fungover.mmotodo.entities.task.Task;
+import org.fungover.mmotodo.services.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+
+@Controller
+public class TaskController {
+    private final TaskService taskService;
+
+    @Autowired
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @QueryMapping
+    public Task taskById(@Argument int id) {
+        return taskService.getTaskById(id);
+    }
+
+    @QueryMapping
+    public List<Task> allTasks() {
+        return taskService.getAllTasks();
+    }
+}
+
---
+++ src/main/java/org/fungover/mmotodo/services/task/TaskService.java
@@ -0,0 +1,26 @@
+package org.fungover.mmotodo.services.task;
+
+import org.fungover.mmotodo.entities.task.Task;
+import org.fungover.mmotodo.repositories.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TaskService {
+    private final TaskRepository taskRepository;
+
+    @Autowired
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public Task getTaskById(int id) {
+        return taskRepository.findById(id).orElseThrow(() -> new RuntimeException("Task not found"));
+    }
+
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+}
