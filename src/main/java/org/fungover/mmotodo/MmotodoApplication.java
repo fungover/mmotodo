b994e4f6ee8b6688af3ec472d3cf145f3e0e5b18
Fabricio Flores <me@fabricioflores.se>
2023-12-08 12:09:16 +0100

    Repository for tasks and categories, added graphql mutations, fixed bug from main.
    Co-authored-by: Jakob Palmborg <112066906+jakobpalmborg@users.noreply.github.com>
    Co-authored-by: Axel Asp <107689239+solidtop@users.noreply.github.com>

--- src/main/java/org/fungover/mmotodo/MmotodoApplication.java
+++ src/main/java/org/fungover/mmotodo/MmotodoApplication.java
@@ -4,9 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class MmotodoApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MmotodoApplication.class, args);
     }
