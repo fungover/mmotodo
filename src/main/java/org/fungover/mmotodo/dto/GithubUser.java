1c9acb788f6d907047c22e818863e1bd1015c6db
Rasmus3liasson <ras.muse@hotmail.com>
2023-12-11 08:52:01 +0100

    route for frontend to access information about autenticated user

---
+++ src/main/java/org/fungover/mmotodo/dto/GithubUser.java
@@ -0,0 +1,10 @@
+package org.fungover.mmotodo;
+
+public record GithubUser(String userName,
+                         String name,
+                         int id,
+                         String avatarUrl,
+                         String githubProfile
+) {
+
+}
