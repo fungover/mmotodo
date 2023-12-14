eac2da716491f6059372d98c919d2440cfeb80a3
Fabricio Flores <32011023+rfabricioflores@users.noreply.github.com>
2023-12-14 10:31:00 +0100

    test for taskUpdate method
    Co-authored-by: Jakob Palmborg <112066906+jakobpalmborg@users.noreply.github.com>
    Co-authored-by:  Axel Asp <107689239+solidtop@users.noreply.github.com>

--- src/main/java/org/fungover/mmotodo/task/TaskUpdateDto.java
+++ src/main/java/org/fungover/mmotodo/task/TaskUpdateDto.java
@@ -2,11 +2,10 @@ package org.fungover.mmotodo.task;
 
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
-import org.springframework.boot.context.properties.bind.DefaultValue;
 
 public record TaskUpdateDto(
         @NotNull @PositiveOrZero
-        int id,
+        Integer id,
         String title,
         String description,
         String status,
