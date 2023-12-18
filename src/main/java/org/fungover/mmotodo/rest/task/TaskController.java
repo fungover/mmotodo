3fc8d24c975522c649f48e14a35f6e679ccd88a9
a-jaxell <97404672+a-jaxell@users.noreply.github.com>
2023-12-18 15:02:25 +0100

    Trim responses

--- src/main/java/org/fungover/mmotodo/rest/task/TaskController.java
+++ src/main/java/org/fungover/mmotodo/rest/task/TaskController.java
@@ -4,7 +4,6 @@ import org.fungover.mmotodo.task.Task;
 import org.fungover.mmotodo.task.TaskCreateDto;
 import org.fungover.mmotodo.task.TaskService;
 import org.fungover.mmotodo.task.TaskUpdateDto;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,42 +20,34 @@ public class TaskController {
     }
 
     @GetMapping("/")
-    public ResponseEntity<List<Task>> getAllTasks(@RequestParam(required = false) Long userId,
-                                                  @RequestParam(required = false) Long teamId) {
-        if (userId != null) {
-            return ResponseEntity.ok(service.findTasksByUserId(userId));
-        } else if (teamId != null) {
-            return ResponseEntity.ok(service.findTasksByTeamId(teamId));
-        }
-        return ResponseEntity.ok(service.getAllTasks());
+    public List<Task> getAllTasks(){
+        return service.getAllTasks();
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Task> getTaskById(@PathVariable int id) {
-        Task task = service.getTaskById(id);
-        return task != null ? ResponseEntity.ok(task) : ResponseEntity.notFound().build();
+    public Task getTaskById(@PathVariable int id) {
+        return service.getTaskById(id);
     }
 
     @PostMapping
-    public ResponseEntity<Task> createTask(@RequestBody TaskCreateDto task) {
-        return ResponseEntity.ok(service.addTask(task));
+    public Task createTask(@RequestBody TaskCreateDto task) {
+        return service.addTask(task);
     }
 
     @PutMapping("")
-    public ResponseEntity<Task> updateTask(@RequestBody TaskUpdateDto task) {
-        return ResponseEntity.ok(service.updateTask(task));
+    public Task updateTask(@RequestBody TaskUpdateDto task) {
+        return service.updateTask(task);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> deleteTask(@PathVariable int id) {
-        try {
+    public ResponseEntity deleteTask(@PathVariable int id) {
 
-            service.deleteTask(id);
-            return ResponseEntity.ok().body("{\"message\": \"Task deleted successfully\"}");
-        }catch(Exception e) {
-            // log exception here
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("{\"error\": \"Error occurred while deleting the task.\"}")
-        }
+        var success = service.deleteTask(id);
+
+        return success ?
+                ResponseEntity.ok("{\"message\": \"Task deleted successfully\"}") :
+                ResponseEntity.unprocessableEntity().body("{\"message\": \"Unable to delete\"}");
+
     }
 }
-}
+
