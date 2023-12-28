916fc9cbc39ac85dc0de000e49bf4e14fecd9e29
Anton Holst <holst9559@hotmail.com>
2023-12-28 11:09:05 +0100

    minor refactor

--- src/main/java/org/fungover/mmotodo/user/User.java
+++ src/main/java/org/fungover/mmotodo/user/User.java
@@ -66,5 +66,4 @@ public class User {
     public final int hashCode() {
         return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
     }
-
 }
