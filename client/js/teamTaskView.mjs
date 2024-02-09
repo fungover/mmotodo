213351736079e1a411678d4fe6f83496b2372e2b
3xer <pontuslundholm1@gmail.com>
2024-02-09 14:08:11 +0100

    rendering of team view done, option to assign user to task in team view.

--- client/js/teamTaskView.mjs
+++ client/js/teamTaskView.mjs
@@ -5,9 +5,9 @@ let data = [
     { col1: '3', col2: 'Create components', col3: '2023-12-18 12:00', col4: 'Create components used in the design', col5: '3', col6: 'Implement', col7: 'Code review', user: 'User 1' },
     { col1: '4', col2: 'Apply design', col3: '2023-12-18 12:00', col4: 'Implement the design', col5: '3', col6: 'Design', col7: 'In progress', user: 'User 2' },
     { col1: '5', col2: 'Load content', col3: '2023-12-20 12:00', col4: 'Load content', col5: '3', col6: 'Design', col7: 'Blocked', user: 'Liber09' },
-    { col1: '6', col2: 'Deploy', col3: '2023-12-24 12:00', col4: 'Deploy everything', col5: '3', col6: 'Deploy', col7: 'ToDo', user: '' },
+    { col1: '6', col2: 'Deploy', col3: '2023-12-24 12:00', col4: 'Deploy everything', col5: '3', col6: 'Deploy', col7: 'ToDo', user: 'unassigned' },
 ];
-
+const users = ['Liber09', 'User 1', 'User 2', 'User 3'];
 
 function renderTeamTable (){
     const tbody = document.getElementById('tbody');
@@ -21,21 +21,49 @@ data.forEach(element => {
     tbody.appendChild(tr)
     const td = document.createElement('td')
     tr.appendChild(td)
-    tr.innerHTML = `<td class="text-center">${element.col1}</td>
+    tr.innerHTML = `
+    <td class="text-center">${element.col1}</td>
     <td>${element.col2}</td>
     <td class="text-center">${element.col3}</td>
     <td>${element.col4}</td>
     <td class="text-center">${element.col5}</td><td class="text-center">${element.col6}</td>
-    <td class="text-nowrap align-middle text-center"> <span class="done">${element.col7}</span> </td>
-    <td class="assigneeSelect">${element.col8}</td>
+    <td class="text-nowrap align-middle text-center"> 
+        <span class="${setTaskStatusStyle(element.col7)}">${element.col7}</span>
+    </td>
+    <td class="assigneeSelect"> 
+        <select id="assigneeElementNr-${element.col1}"> 
+        ${
+           createUserOptions(users)
+        }
+        </select> 
+    </td>
     `;
-     
-    
+    function createUserOptions(us){
+        console.log(us)
+        if(us.length == 4){
+            return us.map((user) => `<option value="${user}">${user}</option>`).join('')
+        }
+       
+    }
+    function setTaskStatusStyle(span) {
+        if (span === 'In progress') {
+            return'inProgress';
+        } else if (span === 'ToDo') {
+            return'toDo';
+        } else if (span === 'Done') {
+            return'done';
+        } else if (span === 'Ready for test') {
+            return'readyForTest';
+        } else if (span === 'Blocked') {
+            return'blocked';
+        } else  {
+            return'codeReview';
+        }
+    }
 
 
 
-
-
+   // const assigneeElement = document.getElementById('assigneeElementNr-'+ element.col1)
     
 });
     
